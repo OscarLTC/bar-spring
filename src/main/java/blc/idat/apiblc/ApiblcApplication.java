bf4ede49023cd6f0411168b1d388511dc76fe2ac
Oscar <oscartaco2003@gmail.com>
2022-06-23 01:05:19 -0500

    first commit

---
+++ src/main/java/blc/idat/apiblc/ApiblcApplication.java
@@ -0,0 +1,13 @@
+package blc.idat.apiblc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiblcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiblcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/blc/idat/apiblc/ApiblcApplicationTests.java
@@ -0,0 +1,13 @@
+package blc.idat.apiblc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiblcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
