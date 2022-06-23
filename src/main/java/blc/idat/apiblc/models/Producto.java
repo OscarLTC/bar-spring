fbc1cc1d22bf0da4cfa66c375f2c1c43c6e5f324
SebastianRodriguez14 <sebastianrodriguezventura@gmail.com>
2022-06-23 18:45:27 -0500

    impln servicio de listado de productos

--- src/main/java/blc/idat/apiblc/models/Producto.java
+++ src/main/java/blc/idat/apiblc/models/Producto.java
@@ -1,4 +1,43 @@
 package blc.idat.apiblc.models;
 
-public class Producto {
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity(name = "Producto")
+@Table(name = "PRODUCTO")
+public class Producto implements Serializable {
+
+    private static final long serialVersion = 1L;
+
+    @Id
+    @Column(name = "COD_PRODUCTO")
+    private long codigo;
+
+    @Column(name = "DESCRIPCION")
+    private String descripcion;
+
+    @Column(name = "PRECIO")
+    private double precio;
+
+    @Column(name = "IMAGEN")
+    private String imagen;
+
+    @Column(name = "ESTADO")
+    private boolean estado;
+
+
+
 }
--- src/main/java/blc/idat/apiblc/rest/ProductoController.java
+++ src/main/java/blc/idat/apiblc/rest/ProductoController.java
@@ -1,4 +1,6 @@
 package blc.idat.apiblc.rest;
 
+
+
 public class ProdcutoController {
 }
