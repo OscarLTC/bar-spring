2c962fc10f93c3e841df3f871b0fe027f72902ea
SebastianRodriguez14 <sebastianrodriguezventura@gmail.com>
2022-07-20 18:36:41 -0500

    add services of update order and custom class for update

--- src/main/java/blc/idat/apiblc/repository/PedidoRepository.java
+++ src/main/java/blc/idat/apiblc/repository/PedidoRepository.java
@@ -5,10 +5,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.sql.Date;
 import java.util.List;
+import java.util.Optional;
 
 public interface PedidoRepository extends JpaRepository<Pedido, Long> {
 
     @Query(value = "select p from Pedido p where p.cliente.codigo = :id")
     List<Pedido> findByCliente(@Param("id") long id);
+
+
 }
